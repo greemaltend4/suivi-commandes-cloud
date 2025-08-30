3b4e1ebba144aa87dd9e658110cfc12b328a4731
greemaltend4 <dengestabiy@hotmail.com>
2025-08-30 00:01:49 -0500

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import CommandList from './components/CommandList';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Suivi des Commandes</h1>
+      <CommandList />
+    </div>
+  );
+}
+
+export default App;
