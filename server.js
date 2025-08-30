47546ec9816ae84081b4d2a09364f93f6a2b7a9f
greemaltend4 <dengestabiy@hotmail.com>
2025-08-30 00:06:28 -0500

    Add error handling middleware for centralized error management

--- server.js
+++ server.js
@@ -24,6 +24,12 @@ app.get('/api/commands', (req, res) => {
   }
 });
 
+// Centralized error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
